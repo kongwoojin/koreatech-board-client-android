dd909f1fe88b8b23350f402ba04f8ed242ff4512
kongwoojin <kongwoojin03@gmail.com>
2022-07-03 01:01:59 +0900

    Fix derp

--- app/src/androidTest/java/com/kongjak/koreatechboard/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kongjak/koreatechboard/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.koreatechcse", appContext.packageName)
+        assertEquals("com.kongjak.koreatechboard", appContext.packageName)
     }
 }
