c2d3063d3c77566d789692629d5bd361f581f2ec
kongwoojin <kongwoojin03@gmail.com>
2022-07-15 00:59:27 +0900

    Add source code info

--- app/src/main/java/com/kongjak/koreatechboard/fragment/SettingsFragment.kt
+++ app/src/main/java/com/kongjak/koreatechboard/fragment/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package com.kongjak.koreatechboard.fragment
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
@@ -18,6 +19,13 @@ class SettingsFragment : PreferenceFragmentCompat() {
         when (preference?.key) {
             "license" -> {
                 startActivity(Intent(context, OssLicensesMenuActivity::class.java))
+            }
+            "source_code" -> {
+                val intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://github.com/kongwoojin/koreatech_board_client_android")
+                )
+                startActivity(intent)
             }
         }
         return super.onPreferenceTreeClick(preference)
