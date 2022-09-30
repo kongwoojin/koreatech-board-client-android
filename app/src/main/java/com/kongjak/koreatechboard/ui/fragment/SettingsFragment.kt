7a5906f1d31cf32ff23588fcc6305eb1266d8807
kongwoojin <kongwoojin03@gmail.com>
2022-10-01 01:19:34 +0900

    Add enquiry in settings

--- app/src/main/java/com/kongjak/koreatechboard/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/com/kongjak/koreatechboard/ui/fragment/SettingsFragment.kt
@@ -1,11 +1,15 @@
 package com.kongjak.koreatechboard.ui.fragment
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import com.google.android.material.snackbar.Snackbar
 import com.kongjak.koreatechboard.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -27,6 +31,25 @@ class SettingsFragment : PreferenceFragmentCompat() {
                     Uri.parse("https://github.com/kongwoojin/koreatech_board_client_android")
                 )
                 startActivity(intent)
+            }
+            "mail" -> {
+                val intent = Intent(Intent.ACTION_SENDTO)
+                intent.data = Uri.parse("mailto:")
+                intent.putExtra(Intent.EXTRA_EMAIL, getString(R.string.mail_address))
+                intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_subject))
+                intent.putExtra(
+                    Intent.EXTRA_TEXT,
+                    getString(R.string.mail_text, Build.MODEL, Build.VERSION.SDK_INT)
+                )
+                try {
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    Snackbar.make(
+                        requireView(),
+                        getString(R.string.mail_app_not_found),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
         }
         return super.onPreferenceTreeClick(preference)
