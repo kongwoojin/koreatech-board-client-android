998f196f396db17e9fcb001d9f8c10e4c10e746c
kongwoojin <kongwoojin03@gmail.com>
2023-06-04 13:24:51 +0900

    feat: Change article api's parameter to uuid

--- data/src/main/java/com/kongjak/koreatechboard/data/api/API.kt
+++ data/src/main/java/com/kongjak/koreatechboard/data/api/API.kt
@@ -2,9 +2,11 @@ package com.kongjak.koreatechboard.data.api
 
 import com.kongjak.koreatechboard.data.model.ArticleResponse
 import com.kongjak.koreatechboard.data.model.BoardResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+import java.util.UUID
 
 interface API {
     @GET("{site}/{board}/")
@@ -17,6 +19,6 @@ interface API {
     @GET("{site}/article/")
     suspend fun getArticle(
         @Path("site") site: String,
-        @Query("url") url: String
+        @Query("uuid") uuid: UUID
     ): ArticleResponse
 }
