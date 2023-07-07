110ae2a1d3b1e295d76a024f6708c20731a4c716
WooJin Kong <kongwoojin03@gmail.com>
2023-07-07 17:21:27 +0900

    feat: Add Compose theme files

---
+++ app/src/main/java/com/kongjak/koreatechboard/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.kongjak.koreatechboard.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val KoreatechMain1 = Color(0xFFFF7F00)
+val KoreatechMain2 = Color(0xFF183072)
+
+val KoreatechSub1 = Color(0xFF22449C)
+val KoreatechSub2 = Color(0xFFB3B3AA)
+val KoreatechSub3 = Color(0xFF4C4C4C)
---
+++ app/src/main/java/com/kongjak/koreatechboard/ui/theme/Theme.kt
@@ -0,0 +1,53 @@
+package com.kongjak.koreatechboard.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+private val DarkColors = darkColors(
+    primary = KoreatechSub1,
+    secondary = KoreatechSub3,
+)
+
+private val LightColors = lightColors(
+    primary = KoreatechMain1,
+    secondary = KoreatechMain1,
+)
+
+@Composable
+fun KoreatechBoardTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val color = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            if (darkTheme) DarkColors else LightColors
+        }
+
+        darkTheme -> DarkColors
+        else -> LightColors
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = color.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colors = color,
+        content = content
+    )
+}
