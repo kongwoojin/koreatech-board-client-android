e03abb0a7bb0c600d2d913213f524b4aea699a58
WooJin Kong <kongwoojin03@gmail.com>
2023-07-08 09:19:18 +0900

    feat: Add ContextUtil

---
+++ app/src/main/java/com/kongjak/koreatechboard/util/ContextUtil.kt
@@ -0,0 +1,16 @@
+package com.kongjak.koreatechboard.util
+
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+
+// from https://github.com/google/accompanist/blob/main/permissions/src/main/java/com/google/accompanist/permissions/PermissionsUtil.kt#L132
+
+fun Context.findActivity(): Activity {
+    var context = this
+    while (context is ContextWrapper) {
+        if (context is Activity) return context
+        context = context.baseContext
+    }
+    throw IllegalStateException("Cannot find Activity")
+}
