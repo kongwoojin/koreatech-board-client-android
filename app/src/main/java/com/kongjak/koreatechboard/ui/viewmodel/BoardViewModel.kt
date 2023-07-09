c257e91ba43a52d35f1cf964092e4d3f0164f05e
WooJin Kong <kongwoojin03@gmail.com>
2023-07-09 14:57:14 +0900

    feat: Modify BoardViewModel to use coroutine flow

--- app/src/main/java/com/kongjak/koreatechboard/ui/viewmodel/BoardViewModel.kt
+++ app/src/main/java/com/kongjak/koreatechboard/ui/viewmodel/BoardViewModel.kt
@@ -4,101 +4,19 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.kongjak.koreatechboard.domain.base.ResponseResult
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.kongjak.koreatechboard.domain.model.BoardData
 import com.kongjak.koreatechboard.domain.usecase.GetBoardUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
 class BoardViewModel @Inject constructor(private val getBoardUseCase: GetBoardUseCase) :
     ViewModel() {
-    private val _boardList = MutableLiveData<List<BoardData>?>()
-    val boardList: LiveData<List<BoardData>?>
-        get() = _boardList
 
-    private val _isLoading = MutableLiveData(false)
-    val isLoading: LiveData<Boolean>
-        get() = _isLoading
+    fun getAPI(site: String, board: String): Flow<PagingData<BoardData>> =
+        getBoardUseCase.execute(site, board).cachedIn(viewModelScope)
 
-    private val _page = MutableLiveData(1)
-    val page: LiveData<Int>
-        get() = _page
-
-    private val _lastPage = MutableLiveData(1)
-    val lastPage: LiveData<Int>
-        get() = _lastPage
-
-    private val _board = MutableLiveData<String>()
-    val board: LiveData<String>
-        get() = _board
-
-    private val _site = MutableLiveData<String>()
-    val site: LiveData<String>
-        get() = _site
-
-    private val _statusCode = MutableLiveData(200)
-    val statusCode: LiveData<Int>
-        get() = _statusCode
-
-    init {
-        _boardList.value = ArrayList()
-    }
-
-    fun nextPage() {
-        if (_page.value!! < _lastPage.value!!) {
-            _page.value = _page.value?.plus(1)
-            getApi()
-        }
-    }
-
-    fun prevPage() {
-        if (_page.value!! > 1) {
-            _page.value = _page.value?.minus(1)
-            getApi()
-        }
-    }
-
-    fun initData() {
-        if (isLoading.value == false && boardList.value?.size == 0) {
-            getApi()
-        }
-    }
-
-    fun setBoardData(board: String) {
-        _board.value = board
-    }
-
-    fun setSiteData(site: String) {
-        _site.value = site
-    }
-
-    fun getApi() {
-        viewModelScope.launch {
-            _isLoading.value = true
-            runCatching {
-                getBoardUseCase.execute(site.value!!, board.value!!, page.value!!)
-            }.onSuccess {
-                when (it) {
-                    is ResponseResult.Success -> {
-                        _lastPage.value = it.data.lastPage
-                        _boardList.value = it.data.boardData
-                        _statusCode.value = it.data.statusCode
-                    }
-                    is ResponseResult.Error -> {
-                        _statusCode.value = it.errorType.statusCode
-                    }
-                }
-            }.onFailure {
-            }
-
-            _isLoading.value = false
-        }
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        _isLoading.value = false
-    }
 }
