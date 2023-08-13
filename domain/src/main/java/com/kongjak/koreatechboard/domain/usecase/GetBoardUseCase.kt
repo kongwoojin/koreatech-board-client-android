8d68356ab432b5d65ccc03a4b38676e0263b6a8e
WooJin Kong <kongwoojin03@gmail.com>
2023-08-13 15:48:43 +0900

    feat: Update GetBoardUseCase

--- domain/src/main/java/com/kongjak/koreatechboard/domain/usecase/GetBoardUseCase.kt
+++ domain/src/main/java/com/kongjak/koreatechboard/domain/usecase/GetBoardUseCase.kt
@@ -1,12 +1,15 @@
 package com.kongjak.koreatechboard.domain.usecase
 
+import androidx.paging.PagingData
 import com.kongjak.koreatechboard.domain.base.ResponseResult
 import com.kongjak.koreatechboard.domain.model.Board
+import com.kongjak.koreatechboard.domain.model.BoardData
 import com.kongjak.koreatechboard.domain.repository.BoardRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetBoardUseCase @Inject constructor(private val boardRepository: BoardRepository) {
-    suspend fun execute(site: String, board: String, page: Int): ResponseResult<Board> {
-        return boardRepository.getBoard(site, board, page)
+    fun execute(site: String, board: String): Flow<PagingData<BoardData>> {
+        return boardRepository.getBoard(site, board)
     }
 }
