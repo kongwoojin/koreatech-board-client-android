bdb8ef95b2feea6c049682e3edeee3ba5b1e349c
WooJin Kong <kongwoojin03@gmail.com>
2023-08-13 15:47:55 +0900

    feat: Add BoardPagingSource

--- data/src/main/java/com/kongjak/koreatechboard/data/datasource/remote/BoardRemoteDataSource.kt
+++ data/src/main/java/com/kongjak/koreatechboard/data/datasource/remote/BoardRemoteDataSource.kt
@@ -6,10 +6,6 @@ import retrofit2.Response
 import javax.inject.Inject
 
 class BoardRemoteDataSource @Inject constructor(private val api: API) {
-    suspend fun getBoard(site: String, board: String, page: Int): Response<BoardResponse> {
-        return api.getBoard(site, board, page)
-    }
-
     suspend fun getBoardMinimum(site: String, board: String): Response<BoardResponse> {
         return api.getBoardMinimum(site, board)
     }
--- data/src/main/java/com/kongjak/koreatechboard/data/repository/BoardRepositoryImpl.kt
+++ data/src/main/java/com/kongjak/koreatechboard/data/repository/BoardRepositoryImpl.kt
@@ -1,17 +1,29 @@
 package com.kongjak.koreatechboard.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.kongjak.koreatechboard.data.api.API
 import com.kongjak.koreatechboard.data.datasource.remote.BoardRemoteDataSource
 import com.kongjak.koreatechboard.data.mapper.BoardMapper
+import com.kongjak.koreatechboard.data.paging.BoardPagingSource
 import com.kongjak.koreatechboard.domain.base.ResponseResult
 import com.kongjak.koreatechboard.domain.model.Board
+import com.kongjak.koreatechboard.domain.model.BoardData
 import com.kongjak.koreatechboard.domain.repository.BoardRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class BoardRepositoryImpl @Inject constructor(private val boardRemoteDataSource: BoardRemoteDataSource) :
+class BoardRepositoryImpl @Inject constructor(private val boardRemoteDataSource: BoardRemoteDataSource, private val api: API) :
     BoardRepository {
-    override suspend fun getBoard(site: String, board: String, page: Int): ResponseResult<Board> {
-        val response = boardRemoteDataSource.getBoard(site, board, page)
-        return BoardMapper.mapToBoard(response.body(), response.code())
+    override fun getBoard(site: String, board: String): Flow<PagingData<BoardData>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { BoardPagingSource(api, site, board) }
+        ).flow
     }
 
     override suspend fun getBoardMinimum(site: String, board: String): ResponseResult<Board> {
