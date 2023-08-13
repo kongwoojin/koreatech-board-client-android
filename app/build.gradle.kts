e838be9e8dba92b4a7df2c896c77cef952e3d737
WooJin Kong <kongwoojin03@gmail.com>
2023-08-13 15:46:23 +0900

    feat: Update material3 compose library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,7 +101,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3-android:1.2.0-alpha02")
     implementation("androidx.compose.runtime:runtime-livedata:1.5.0")
     implementation("androidx.navigation:navigation-compose:2.6.0")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
