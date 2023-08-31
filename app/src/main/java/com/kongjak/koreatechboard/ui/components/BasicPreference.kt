9f6c2c8e1b0d0ea0c319f1c6e8a161d5ce31e15d
WooJin Kong <kongwoojin03@gmail.com>
2023-08-31 13:58:46 +0900

    feat: Add BasicPreference component

---
+++ app/src/main/java/com/kongjak/koreatechboard/ui/components/BasicPreference.kt
@@ -0,0 +1,137 @@
+package com.kongjak.koreatechboard.ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BasicPreference(
+    modifier: Modifier = Modifier,
+    title: String,
+    onClick: () -> Unit,
+    content: @Composable (() -> Unit)? = null
+) {
+    Column(modifier = modifier.clickable { onClick() }) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(modifier = Modifier.weight(1f), text = title)
+            Column(horizontalAlignment = Alignment.End) {
+                if (content != null) {
+                    content()
+                }
+            }
+        }
+        Divider(color = Color.Gray, thickness = 0.5.dp)
+    }
+}
+
+@Composable
+fun BasicPreference(
+    modifier: Modifier = Modifier,
+    title: String,
+    summary: String,
+    onClick: () -> Unit,
+    content: @Composable (() -> Unit)? = null
+) {
+    Column(modifier = modifier.clickable { onClick() }) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = title)
+                Text(text = summary, fontSize = MaterialTheme.typography.bodySmall.fontSize)
+            }
+            Column(horizontalAlignment = Alignment.End) {
+                if (content != null) {
+                    content()
+                }
+            }
+        }
+        Divider(color = Color.Gray, thickness = 0.5.dp)
+    }
+}
+
+@Composable
+fun BasicPreference(
+    modifier: Modifier = Modifier,
+    icon: ImageVector,
+    title: String,
+    onClick: () -> Unit,
+    content: @Composable (() -> Unit)? = null
+) {
+    Column(modifier = modifier.clickable { onClick() }) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = title,
+                modifier = Modifier.padding(end = 8.dp)
+            )
+            Text(modifier = Modifier.weight(1f), text = title)
+            Column(horizontalAlignment = Alignment.End) {
+                if (content != null) {
+                    content()
+                }
+            }
+        }
+        Divider(color = Color.Gray, thickness = 0.5.dp)
+    }
+}
+
+@Composable
+fun BasicPreference(
+    modifier: Modifier = Modifier,
+    icon: ImageVector,
+    title: String,
+    summary: String,
+    onClick: () -> Unit,
+    content: @Composable (() -> Unit)? = null
+) {
+    Column(modifier = modifier.clickable { onClick() }) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = title,
+                modifier = Modifier.padding(end = 8.dp)
+            )
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = title)
+                Text(text = summary, fontSize = MaterialTheme.typography.bodySmall.fontSize)
+            }
+            Column(horizontalAlignment = Alignment.End) {
+                if (content != null) {
+                    content()
+                }
+            }
+        }
+        Divider(color = Color.Gray, thickness = 0.5.dp)
+    }
+}
--- app/src/main/java/com/kongjak/koreatechboard/ui/components/Preference.kt
+++ app/src/main/java/com/kongjak/koreatechboard/ui/components/Preference.kt
@@ -1,23 +1,12 @@
 package com.kongjak.koreatechboard.ui.components
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun Preference(
@@ -25,17 +14,7 @@ fun Preference(
     title: String,
     onClick: () -> Unit
 ) {
-    Column(modifier = modifier.clickable { onClick() }) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(text = title)
-        }
-        Divider(color = Color.Gray, thickness = 0.5.dp)
-    }
+    BasicPreference(modifier = modifier, title = title, onClick = { onClick() })
 }
 
 @Composable
@@ -45,20 +24,7 @@ fun Preference(
     summary: String,
     onClick: () -> Unit
 ) {
-    Column(modifier = modifier.clickable { onClick() }) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Column {
-                Text(text = title)
-                Text(text = summary, fontSize = MaterialTheme.typography.bodySmall.fontSize)
-            }
-        }
-        Divider(color = Color.Gray, thickness = 0.5.dp)
-    }
+    BasicPreference(modifier = modifier, title = title, summary = summary, onClick = { onClick() })
 }
 
 @Composable
@@ -68,22 +34,7 @@ fun Preference(
     title: String,
     onClick: () -> Unit
 ) {
-    Column(modifier = modifier.clickable { onClick() }) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                imageVector = icon,
-                contentDescription = title,
-                modifier = Modifier.padding(end = 8.dp)
-            )
-            Text(text = title)
-        }
-        Divider(color = Color.Gray, thickness = 0.5.dp)
-    }
+    BasicPreference(modifier = modifier, icon = icon, title = title, onClick = { onClick() })
 }
 
 @Composable
@@ -94,25 +45,7 @@ fun Preference(
     summary: String,
     onClick: () -> Unit
 ) {
-    Column(modifier = modifier.clickable { onClick() }) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                imageVector = icon,
-                contentDescription = title,
-                modifier = Modifier.padding(end = 8.dp)
-            )
-            Column {
-                Text(text = title)
-                Text(text = summary, fontSize = MaterialTheme.typography.bodySmall.fontSize)
-            }
-        }
-        Divider(color = Color.Gray, thickness = 0.5.dp)
-    }
+    BasicPreference(modifier = modifier, icon = icon, title = title, summary = summary, onClick = { onClick() })
 }
 
 @Preview
