5b1c970a7198ef3a718fdac125f49229ba9df66a
WooJin Kong <kongwoojin03@gmail.com>
2023-09-03 08:10:50 +0900

    feat: Show no result when search result is empty

--- app/src/main/java/com/kongjak/koreatechboard/ui/search/Search.kt
+++ app/src/main/java/com/kongjak/koreatechboard/ui/search/Search.kt
@@ -3,6 +3,7 @@ package com.kongjak.koreatechboard.ui.search
 import android.content.Intent
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -61,13 +62,24 @@ fun SearchScreen(site: String, board: String, title: String) {
             )
         },
         content = { contentPadding ->
-            SearchContent(contentPadding = contentPadding, site = site, board = board, title = title)
+            SearchContent(
+                contentPadding = contentPadding,
+                site = site,
+                board = board,
+                title = title
+            )
         }
     )
 }
 
 @Composable
-fun SearchContent(searchViewModel: SearchViewModel = hiltViewModel(), contentPadding: PaddingValues, site: String, board: String, title: String) {
+fun SearchContent(
+    searchViewModel: SearchViewModel = hiltViewModel(),
+    contentPadding: PaddingValues,
+    site: String,
+    board: String,
+    title: String
+) {
     val context = LocalContext.current
 
     val lazyPostList =
@@ -124,6 +136,12 @@ fun SearchContent(searchViewModel: SearchViewModel = hiltViewModel(), contentPad
                     val errorMessage =
                         (loadState.append as LoadState.Error).error.localizedMessage
                     item { BoardError(errorMessage) }
+                }
+
+                loadState.refresh is LoadState.NotLoading -> {
+                    if (lazyPostList.itemCount == 0) {
+                        item { Text(text = stringResource(id = R.string.search_no_result)) }
+                    }
                 }
             }
         }
