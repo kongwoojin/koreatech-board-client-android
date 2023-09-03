eb3346f5559cc4581511a3d13b965f0b691a8c34
WooJin Kong <kongwoojin03@gmail.com>
2023-09-03 13:15:18 +0900

    fix: Downgrade material3 library to 1.2.0-alpha04
    * BottomSheet not working at > 1.2.0-alpha05

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,7 +104,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3-android:1.2.0-alpha06")
+    implementation("androidx.compose.material3:material3-android:1.2.0-alpha04")
     implementation("androidx.compose.runtime:runtime-livedata:1.5.0")
     implementation("androidx.navigation:navigation-compose:2.7.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
