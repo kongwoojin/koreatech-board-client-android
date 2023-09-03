5b184336bd6968fdeb0de3c0725db6891f9eb551
WooJin Kong <kongwoojin03@gmail.com>
2023-09-03 13:00:51 +0900

    chore: Update libraries and remove unused libraries

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -46,14 +46,12 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.6.1")
-    implementation("androidx.paging:paging-common:3.2.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.3")
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.paging:paging-common-ktx:3.2.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     implementation("com.google.dagger:hilt-android:2.46.1")
     kapt("com.google.dagger:hilt-android-compiler:2.46.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
