efdfc7094f68cbc02ebde0deb4fd81464478b2d9
WooJin Kong <kongwoojin03@gmail.com>
2023-09-03 10:47:16 +0900

    chore: Cleanup BoardViewModel

--- app/src/main/java/com/kongjak/koreatechboard/ui/board/BoardViewModel.kt
+++ app/src/main/java/com/kongjak/koreatechboard/ui/board/BoardViewModel.kt
@@ -1,16 +1,12 @@
 package com.kongjak.koreatechboard.ui.board
 
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.kongjak.koreatechboard.domain.model.BoardData
 import com.kongjak.koreatechboard.domain.usecase.GetBoardUseCase
-import com.kongjak.koreatechboard.util.routes.Department
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -23,8 +19,4 @@ class BoardViewModel @Inject constructor(private val getBoardUseCase: GetBoardUs
 
     fun getAPI(site: String, board: String): Flow<PagingData<BoardData>> =
         getBoardUseCase(site, board).cachedIn(viewModelScope)
-
-    fun changeIndex(index: Int) {
-        tabIndex.intValue = index
-    }
 }
