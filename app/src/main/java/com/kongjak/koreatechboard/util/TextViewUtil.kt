f533e8f8f81484fca847d3977db77cca13ecfe5f
WooJin Kong <kongwoojin03@gmail.com>
2023-10-06 16:58:52 +0900

    chore: Remove unused htmlText

--- app/src/main/java/com/kongjak/koreatechboard/util/TextViewUtil.kt
+++ app/src/main/java/com/kongjak/koreatechboard/util/TextViewUtil.kt
@@ -1,9 +1,7 @@
 package com.kongjak.koreatechboard.util
 
-import android.text.Html
 import android.text.util.Linkify
 import android.widget.TextView
-import androidx.core.text.HtmlCompat
 import com.kongjak.koreatechboard.domain.model.Files
 import java.util.regex.Pattern
 
@@ -26,21 +24,5 @@ var TextView.fileText: ArrayList<Files>?
                     Pattern.compile(file.fileName.replace("(", "\\(").replace(")", "\\)"))
                 Linkify.addLinks(this, pattern, file.fileUrl, null, transform)
             }
-        }
-    }
-
-var TextView.htmlText: String?
-    get() {
-        return null
-    }
-    set(value) {
-        if (value != null) {
-            val htmlText = Html.fromHtml(
-                value.toString(),
-                HtmlCompat.FROM_HTML_MODE_COMPACT,
-                GlideImageGetter(this),
-                null
-            )
-            text = htmlText
         }
     }
