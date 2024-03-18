0dd8bbba40adad1002c040218bf60b700eee43eb
WooJin Kong <kongwoojin03@gmail.com>
2024-03-18 11:00:51 +0900

    fix: Fix NPE on Home

--- app/src/main/java/com/kongjak/koreatechboard/ui/home/HomeBoardState.kt
+++ app/src/main/java/com/kongjak/koreatechboard/ui/home/HomeBoardState.kt
@@ -6,7 +6,7 @@ import com.kongjak.koreatechboard.ui.base.UiState
 data class HomeBoardState(
     val isLoaded: Boolean = false,
     val isSuccess: Boolean = false,
-    val boardList: Map<String, List<BoardData>> = emptyMap(),
+    val boardList: Map<String, List<BoardData>?> = emptyMap(),
     val statusCode: Int = 0,
     val error: String = ""
 ) : UiState
--- app/src/main/java/com/kongjak/koreatechboard/ui/home/HomeBoardViewModel.kt
+++ app/src/main/java/com/kongjak/koreatechboard/ui/home/HomeBoardViewModel.kt
@@ -33,7 +33,7 @@ class HomeBoardViewModel @Inject constructor(private val getBoardMinimumUseCase:
                                 setState(
                                     oldState.copy(
                                         isSuccess = true,
-                                        boardList = (oldState.boardList + (event.board to (it.data.boardData)!!)),
+                                        boardList = (oldState.boardList + (event.board to (it.data.boardData))),
                                         statusCode = it.data.statusCode,
                                         isLoaded = true
                                     )
