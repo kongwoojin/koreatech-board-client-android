25bd4376603e550b1e48b75152c075314b2bdc62
WooJin Kong <kongwoojin03@gmail.com>
2024-03-29 15:31:56 +0900

    feat: Add api constraints

---
+++ data/src/main/java/com/kongjak/koreatechboard/data/api/APIConstraints.kt
@@ -0,0 +1,9 @@
+package com.kongjak.koreatechboard.data.api
+
+object APIConstraints {
+    object V3 {
+        const val BOARD = "{site}/{board}"
+        const val SEARCH_WITH_TITLE = "{site}/{board}/search/title"
+        const val ARTICLE = "article"
+    }
+}
