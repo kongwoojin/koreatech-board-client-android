2e83bd6a58937804853cd80bf377c77dac194853
WooJin Kong <kongwoojin03@gmail.com>
2024-03-29 11:29:40 +0900

    chore: Remove old pullrefresh library from settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,11 +12,7 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-includeBuild("libs/androidx-compose-material3-pullrefresh-2023.03.00") {
-    dependencySubstitution {
-        substitute(module("me.omico.lux:lux-androidx-compose-material3-pullrefresh")).using(project(":library"))
-    }
-}
+
 rootProject.name = "Koreatech Board"
 include(":app")
 include(":domain")
