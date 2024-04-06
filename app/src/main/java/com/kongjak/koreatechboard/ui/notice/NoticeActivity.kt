eaffc1965841f2ef11b970029ed7f093766f98bd
WooJin Kong <kongwoojin03@gmail.com>
2024-04-06 11:32:49 +0900

    feat: Apply theme to NoticeActivity

--- app/src/main/java/com/kongjak/koreatechboard/ui/notice/NoticeActivity.kt
+++ app/src/main/java/com/kongjak/koreatechboard/ui/notice/NoticeActivity.kt
@@ -3,6 +3,8 @@ package com.kongjak.koreatechboard.ui.notice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -16,23 +18,33 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import com.kongjak.koreatechboard.R
 import com.kongjak.koreatechboard.ui.theme.KoreatechBoardTheme
+import com.kongjak.koreatechboard.ui.viewmodel.ThemeViewModel
 import com.kongjak.koreatechboard.util.findActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 @OptIn(ExperimentalMaterial3Api::class)
 class NoticeActivity : ComponentActivity() {
+    private val themeViewModel: ThemeViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val context = LocalContext.current
 
-            KoreatechBoardTheme {
+            val isDynamicColor by themeViewModel.isDynamicTheme.observeAsState(true)
+            val isDarkTheme by themeViewModel.isDarkTheme.observeAsState()
+
+            KoreatechBoardTheme(
+                dynamicColor = isDynamicColor,
+                darkTheme = isDarkTheme ?: isSystemInDarkTheme()
+            ) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
