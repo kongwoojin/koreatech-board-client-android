4a24575b27683ba558f104246bd84e3b6cdcb6a1
WooJin Kong <kongwoojin03@gmail.com>
2024-04-20 10:06:24 +0900

    Remove collectSideEffect in Toolbar

--- app/src/main/java/com/kongjak/koreatechboard/ui/article/ArticleActivity.kt
+++ app/src/main/java/com/kongjak/koreatechboard/ui/article/ArticleActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -22,7 +21,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
@@ -81,9 +79,6 @@ fun Toolbar(
     department: String,
     uuid: UUID
 ) {
-    articleViewModel.collectSideEffect {
-        articleViewModel.handleSideEffect(it)
-    }
     val uiState by articleViewModel.collectAsState()
     val articleUrl = uiState.url
     val isDynamicColor by themeViewModel.isDynamicTheme.observeAsState(true)
