6701aebdc51525e1f9f276667d5d31a9447f9f30
WooJin Kong <kongwoojin03@gmail.com>
2024-05-25 16:43:22 +0900

    feat: Set android build_type key for firebase

--- app/src/androidMain/kotlin/com/kongjak/koreatechboard/MainActivity.kt
+++ app/src/androidMain/kotlin/com/kongjak/koreatechboard/MainActivity.kt
@@ -3,6 +3,9 @@ package com.kongjak.koreatechboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.crashlytics.setCustomKeys
+import com.google.firebase.ktx.Firebase
 import com.kongjak.koreatechboard.di.appModule
 import com.kongjak.koreatechboard.di.databaseModule
 import com.kongjak.koreatechboard.di.networkModule
@@ -18,6 +21,11 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val crashlytics = Firebase.crashlytics
+        crashlytics.setCustomKeys {
+            key("build_type", BuildConfig.BUILD_TYPE)
+        }
 
         setContent {
             KoinApplication(application = {
